e8aebb7f287e26b33a8acacf92ed45a68ee9af80
pc-dedelhart <david.edelhart@personalcapital.com>
2013-02-07 10:20:30 -0800

    extracting canvas library - renaming mis-named node nom module

--- index.js
+++ index.js
@@ -13,24 +13,5 @@ module.exports = {
 	util: {
 		Vector3:      require('./lib/util/vector3'),
 		cross_vector: require('./lib/util/cross_vector')
-	},
-
-	image_file_topos: function (image_files, cb) {
-		var gate = Gate.create();
-
-		var TopoGrid = require('./lib/TopoGrid');
-
-		_.each(image_files, function (file) {
-			TopoGrid({}, {source: file, source_type: 'image_file'}, gate.latch());
-		});
-
-		gate.await(function (err, topo_grids) {
-			cb(null, _.reduce(_.values(topo_grids),
-				function (out, props) {
-					if (props[0]) throw props[0];
-					out.push(props[1]);
-					return out;
-				}, []));
-		});
 	}
 };
